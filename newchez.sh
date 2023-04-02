871f92e438ddb79cfbfe59796da1835f41050b27
Bryan Snyder <snyderb.de@gmail.com>
2023-04-02 13:34:47 -0400

    printf shellcheck issues fixed

--- newchez.sh
+++ newchez.sh
@@ -8,47 +8,47 @@ RESET="\033[0m"
 # Function to prompt the user
 prompt_continue() {
   while true; do
-    printf "${YELLOW}Do you want to continue? [y/n]: ${RESET}"
+    printf '%sDo you want to continue? [y/n]: %s' "$YELLOW" "$RESET"
     read -r choice
     case $choice in
       [Yy]* ) return 0;;
       [Nn]* ) return 1;;
-      * ) printf "${YELLOW}Please answer y(es) or n(o).${RESET}\n";;
+      * ) printf '%sPlease answer y(es) or n(o).%s\n' "$YELLOW" "$RESET";;
     esac
   done
 }
 
-printf "${GREEN}Running 'chezmoi re-add'...${RESET}\n"
+printf '%sRunning '\''chezmoi re-add'\''...%s\n' "$GREEN" "$RESET"
 chezmoi re-add
 if prompt_continue; then
-  printf "${GREEN}Running 'chezmoi apply'...${RESET}\n"
+  printf '%sRunning '\''chezmoi apply'\''...%s\n' "$GREEN" "$RESET"
   chezmoi apply
   if prompt_continue; then
-    printf "${GREEN}Changing directory to chezmoi's git repository...${RESET}\n"
+    printf '%sChanging directory to chezmoi'\''s git repository...%s\n' "$GREEN" "$RESET"
     cd "$HOME/.local/share/chezmoi" || exit 1
 
-    printf "${GREEN}Running 'git status'...${RESET}\n"
+    printf '%sRunning '\''git status'\''...%s\n' "$GREEN" "$RESET"
     git status
     if prompt_continue; then
-      printf "${YELLOW}Enter the commit message: ${RESET}"
+      printf '%sEnter the commit message: %s' "$YELLOW" "$RESET"
       read -r commit_message
 
-      printf "${GREEN}Running 'git commit'...${RESET}\n"
+      printf '%sRunning '\''git commit'\''...%s\n' "$GREEN" "$RESET"
       git commit -m "$commit_message"
       if prompt_continue; then
-        printf "${YELLOW}Do you want to push the commit? [y/n]: ${RESET}"
+        printf '%sDo you want to push the commit? [y/n]: %s' "$YELLOW" "$RESET"
         read -r push_choice
         case $push_choice in
           [Yy]* )
-            printf "${GREEN}Running 'git push'...${RESET}\n"
+            printf '%sRunning '\''git push'\''...%s\n' "$GREEN" "$RESET"
             git push origin;;
           [Nn]* )
-            printf "${GREEN}Commit has been updated locally. You can push it later.${RESET}\n";;
-          * ) printf "${YELLOW}Invalid choice. Commit has been updated locally. You can push it later.${RESET}\n";;
+            printf '%sCommit has been updated locally. You can push it later.%s\n' "$GREEN" "$RESET";;
+          * ) printf '%sInvalid choice. Commit has been updated locally. You can push it later.%s\n' "$YELLOW" "$RESET";;
         esac
       fi
     fi
   fi
 else
-  printf "${GREEN}Aborted.${RESET}\n"
+  printf '%sAborted.%s\n' "$GREEN" "$RESET"
 fi
